bf6fe534dbcb921e960e417e2f6d18c30c28d1f3
elquanted-og <druzhinin.199824@gmail.com>
2025-10-18 12:20:23 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+ bin/pip install Pyyaml==5.4 --no-build-isolation
+ bin/pip install docker-compose
+
+bin/pip install requests==2.26
+
+ bin/pip install urllib3==1.26.0
+bin/pip install urllib3 --upgrade
+/bin/pip install docker==6.1
